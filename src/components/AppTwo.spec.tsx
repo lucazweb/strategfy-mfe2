9f3f8696a6f0e1b84f35435eff2f98f7ed43fbe2
Lucas Cardoso Santos <lucas.casantos@b2wdigital.com>
2022-12-16 23:41:23 -0300

    test: add test file

--- src/components/AppTwo.spec.tsx
+++ src/components/AppTwo.spec.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import AppTwo from "./AppTwo";
-
-afterEach(cleanup);
 
 it("Render AppTwo component correctly", () => {
   render(<AppTwo />);
